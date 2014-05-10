4a1a439f8e107b8fe06555611eff44d43c6061c5
steve <dkong@debian.debiandomain>
2014-05-09 21:11:45 -0400

    broke a bunch of shit in the craigslist.py file :(

--- craigslist/craigslist.py
+++ craigslist/craigslist.py
@@ -10,8 +10,10 @@ TODOS:
 2. add category selection, for now it's searching through "all for sale"
 3. implement state and all states
 4. implement different output types - terminal, html page, xml, json?, idk?
-5. move to an argparse implementation over optparse
+5. maybe add user agent selection/spoofing?
+6. move to an argparse implementation over optparse - Python 3
 """
+
 # *********** Subprocedures ***********************
 # Uses urllib2 to request a call to the
 # web API to get the geolocation data.
@@ -23,27 +25,25 @@ def getResponse(url):
 		sys.exit(-1)
 	return response
 
-#******************************************************
-# Create the option parser.
-# OptionParser( sample usage text)
+# ******************************************************
+# Create the option parser. OptionParser( sample usage text)
 parser = optparse.OptionParser('python craigslist.py -t searchterms <options>')
 
 # (option flag, variable destination, [action to take], type of the variable, help text)
-parser.add_option('-s', dest='state', type='string', help='Specify what state to search in. Optional - Default is Ohio. Type \'all\' to search through all states [not recommended, slow]', default='ohio')
 parser.add_option('-c', dest='city', type='string', help='Specify city to search in. Comma separated. Optional - Default is Columbus. Type \'all\' to search through all cities of the selected state.', default='columbus')
+parser.add_option('-s', dest='state', type='string', help='Specify what state to search in. Optional - Default is Ohio. Type \'all\' to search through all states [not recommended, slow]', default='ohio')
 #parser.add_option('-a', dest='category', type='string', help='Specify which category to search within. Optional - Default is all categories', default='sss'
 parser.add_option('-t', dest='searchterm', type='string', help='Type search terms here, comma separated. Required.')
 parser.add_option('-m', dest='maxprice', type='int', help='Specify max price. Optional.')
 parser.add_option('-n', dest='minprice', type='int', help='Specify min price. Optional.')
-parser.add_option('-r', dest='numresults', type='int', help='Set max amount of results. Optional.')
-parser.add_option('-o', dest='outputType', type='string', help='Set the output type: terminal or html. Optional. Default is print to console.')
+#parser.add_option('-r', dest='numresults', type='int', help='Set max amount of results. Optional.')
+#parser.add_option('-o', dest='outputType', type='string', help='Set the output type: terminal or html. Optional. Default is print to console.')
 parser.add_option('-p', dest='pic', action='store_true', help='Flag this if you want pics required. Default is false.', default = False)
-parser.add_option('-v', dest='verbose', action='store_true', help='Set verbose output. Default is verbose turned on.', default = True)
-parser.add_option('-q', dest='verbose', action='store_false', help='Set quiet output.')
+parser.add_option('-v', dest='verbose', action='store_true', help='Set verbose output. Default is quiet output.', default = False)
 
 (options, args) = parser.parse_args()
 
-# *******************************************************************
+# *********************** MAIN *************************************
 # Main Program is starting here: 
 if options.verbose:
     # print all excess/non parsed arguments
@@ -61,45 +61,66 @@ if options.searchterm == None:
 if options.city == 'all':
 	print "TODO"
 
+if options.state == 'all':
+    print "TODO"
+    # scrape the page of all the city names and shit
+
 # building the url: http://city.craigslist.org/search/category
 # ex: http://columbus.craigslist.org/search/sss?zoomToPosting=&catAbb=sss&query=testingtesting&minAsk=&maxAsk=&hasPic=1
-# categories = sss is all cats
+# categories = sss is all categories
 
+# Grab the search terms and html encode them:
+# There's probably a better way lol
 searchTerms = str(options.searchterm)
 if re.search(",", searchTerms):
-	searchTerms = string.replace(searchTerms, ',', '%20')
+	searchTerms = string.replace(searchTerms, ',', '+')
 # print searchTerms
 
-url = 'http://' + options.city + '.craigslist.org/search/sss?zoomToPosting=&catAbb=sss&query=' + searchTerms + '&minAsk='
+# Building the URL: 
+url = 'http://' + str(options.city) + '.craigslist.org/search/sss?zoomToPosting=&catAbb=sss&query=' + searchTerms + '&minAsk='
 
-if options.minprice != None:
-	url = url + str(options.minprice)
+if options.minprice: url += str(options.minprice)
 
-if options.maxprice != None:
-	url = url + '&maxAsk=' + str(options.maxprice)
-else:
-	url = url + '&maxAsk='
+url += '&maxAsk='
+
+if options.maxprice: url += str(options.maxprice)
 
 if options.pic:
-	if options.verbose: print "pictures required"
-	url = url + '&hasPic=1'
+	if options.verbose: print "Pictures required."
+	url += '&hasPic=1'
 
-print "\nSearching " + url
+if options.verbose: print "\nSearching " + url
+
 craigsResponse = getResponse(url)	 # Making the Web Call
 soup = BeautifulSoup(craigsResponse) # Creating a Beautifulsoup object
 # print soup.prettify()  			 # Pretty Printing the output HTML
 
 # Pulling All Links
-count = 0
+"""
+links = [link.get('href') for link in soup.find_all('a')]
+print set(links)
+for link in set(links):
+    if not re.search("/\w+/\d+.html", link.get('href')):
+        continue	# Skipping shit links    
+"""
+
+"""
+TODO SOMETHING IS BROKEN HERE
+File "craigslist.py", line 108, in <module>
+    if not re.search("/\w+/\d+.html", link.get('href')):
+  File "/usr/lib/python2.6/re.py", line 142, in search
+    return _compile(pattern, flags).search(string)
+TypeError: expected string or buffer
+"""
 for link in soup.find_all('a'):
-	if options.numresults != None:
-		if count > options.numresults: break
-	if not re.search("/\w+/\d+.html", link.get('href')):
-		continue	# Skipping shit links
+    #if not re.search("/\w+/\d+.html", link.get('href')):
+    #    continue	# Skipping shit links
 	print "\n" + link.get_text()
 	print "http://" + options.city + ".craigslist.org" + link.get('href')
-	count = count + 1
-
 print "\n"
 
-# for item in soup.find_all("p", class_="row")
+""" Burial grounds:
+if options.numresults:
+        if count > options.numresults: break
+    # Break if over the specified result limit
+"""
