22852142bac3bbbc4118d30ef0253a0668a357ce
steve <dkong@debian.debiandomain>
2014-05-11 14:48:50 -0400

    major craigslist improvements

--- craigslist/craigslist.py
+++ craigslist/craigslist.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python
 
-import urllib2, sys, optparse, re, string
+import urllib2, sys, optparse, re
 from bs4 import BeautifulSoup
 
 """
 TODOS:
-1. implement all city and multi-city searching
-2. add category selection, for now it's searching through "all for sale"
-3. implement state and all states
-4. implement different output types - terminal, html page, xml, json?, idk?
-5. maybe add user agent selection/spoofing?
-6. move to an argparse implementation over optparse - Python 3
+1. implement multi-city searching
+2. implement all city searching
+3. add category selection, for now it's searching through "all for sale"
+- implement different output types - terminal, html page, xml, json?, idk?
+- move to an argparse implementation over optparse - Python 3
+
+Notes: 
+# building the url: http://city.craigslist.org/search/category
+# ex: http://columbus.craigslist.org/search/sss?zoomToPosting=&catAbb=sss&query=testingtesting&minAsk=&maxAsk=&hasPic=1
+# categories = sss is all categories
 """
 
-# *********** Subprocedures ***********************
-# Uses urllib2 to request a call to the
-# web API to get the geolocation data.
-def getResponse(url, userAgent = None):
+# ***************** Subprocedures *********************
+def getResponse(url):
     opener = urllib2.build_opener()
-    if not userAgent:
-        uAgent = 'User-agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en; rv:1.8.1.11) Gecko/20121207 Firefox/3.5.16'
-    else: uAgent = userAgent
-    opener.addheaders = [(uAgent)]
+    opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en; rv:1.8.1.11) Gecko/20121207 Firefox/3.5.16')]
     try:
         response = opener.open(url).read()
     except:
@@ -31,53 +30,49 @@ def getResponse(url, userAgent = None):
 
 # ******************************************************
 # Create the option parser. OptionParser( sample usage text)
-parser = optparse.OptionParser('python craigslist.py -t searchterms <options>')
+parser = optparse.OptionParser('python craigslist.py -s searchterms <options>')
 
 # (option flag, variable destination, [action to take], type of the variable, help text)
 parser.add_option('-c', dest='city', type='string', help='Specify city (or cities) to search in. Comma separated! Optional - Default is Columbus. Type \'all\' to search through all cities.', default='columbus')
-parser.add_option('-s', dest='searchterm', type='string', help='Type search terms here, comma separated. Required.')
-parser.add_option('-m', dest='maxprice', type='int', help='Specify max price. Optional.')
-parser.add_option('-n', dest='minprice', type='int', help='Specify min price. Optional.')
+parser.add_option('-m', dest='maxprice', type='int', help='Specify max price - number only. Optional.')
+parser.add_option('-n', dest='minprice', type='int', help='Specify min price - number only. Optional.')
 parser.add_option('-p', dest='pic', action='store_true', help='Flag this if you want pics required. Default is false.', default = False)
+parser.add_option('-s', dest='searchterms', type='string', help='Type search terms here, comma separated or put terms in quotes. Required.')
 parser.add_option('-v', dest='verbose', action='store_true', help='Set verbose output. Default is quiet output.', default = False)
-# parser.add_option('-a', dest='category', type='string', help='Specify which category to search within. Optional - Default is all categories', default='sss'
-#parser.add_option('-u', dest='userAgent', type='string', help='Input a custom User-Agnent string.')
 #parser.add_option('-r', dest='numresults', type='int', help='Set max amount of results. Optional.')
+# parser.add_option('-a', dest='category', type='string', help='Specify which category to search within. Optional - Default is all categories', default='sss'
 #parser.add_option('-o', dest='outputType', type='string', help='Set the output type: terminal or html. Optional. Default is print to console.')
 
 (options, args) = parser.parse_args()
 
-# *********************** MAIN *************************************
-# Main Program is starting here: 
+# *********************** Main Program ***************************
 if options.verbose:
     # print all excess/non parsed arguments
-	for arg in args:
-		print "Arg = " + arg
+	for arg in args: print "Arg = " + arg
 
-# Pull a list of cities, should be slurping this with bsoup
-ohioCities = ['akroncanton', 'ashtabula', 'athens', 'chillicothe', 'cincinnati', 'cleveland', 'columbus', 'dayton', 'limaohio', 'mansfield', 'sandusky', 'toledo', 'tuscarawas', 'youngstown', 'zanesville']
-
-if options.searchterm == None:
-	print "Error, please provide search term(s)."
+if not options.searchterms:
+	print "\nError, type in 'python craigslist.py -h' for more help."
 	print parser.print_usage()
 	sys.exit(-1)
 
+# Parse City (or cities):
 if options.city == 'all':
 	print "This is not implemented yet."
+elif re.search("\d+", options.city):
+    print "Invalid city - no numbers in cities!"
+    sys.exit(-1)
+else:
+    userCiti = str(options.city).strip().lower()
 
-# building the url: http://city.craigslist.org/search/category
-# ex: http://columbus.craigslist.org/search/sss?zoomToPosting=&catAbb=sss&query=testingtesting&minAsk=&maxAsk=&hasPic=1
-# categories = sss is all categories
-
-# Grab the search terms and html encode them:
-# There's probably a better way lol
-searchTerms = str(options.searchterm)
+# Parse Search Terms and HTML encode them:
+searchTerms = str(options.searchterms)
 if re.search(",", searchTerms):
-	searchTerms = string.replace(searchTerms, ',', '+')
-# print searchTerms
+	searchTerms = searchTerms.replace(',', '+')
+if re.search(" ", searchTerms):
+    searchTerms = searchTerms.replace(' ', '+')
 
 # Building the URL: 
-url = 'http://' + str(options.city) + '.craigslist.org/search/sss?zoomToPosting=&catAbb=sss&query=' + searchTerms + '&minAsk='
+url = 'http://' + userCiti + '.craigslist.org/search/sss?zoomToPosting=&catAbb=sss&query=' + searchTerms + '&minAsk='
 
 if options.minprice: url += str(options.minprice)
 
@@ -97,7 +92,6 @@ soup = BeautifulSoup(craigsResponse) # Creating a Beautifulsoup object
 
 # Pulling All Links
 # links = [link for link in soup.find_all('a')]
-
 for link in soup.find_all('a'):
     #print type(link), link                          # type = bs4.element.Tag
     #print type(link.get('href')), link.get('href')  # type = unicode
@@ -106,7 +100,10 @@ for link in soup.find_all('a'):
     if not link.get('href') or not link.get_text() or (re.search("^\$", link.get_text())) or (not re.search(".html", link.get('href'))):
         continue
     print '\n' + link.get_text()
-    print "http://" + options.city + ".craigslist.org" + link.get('href')
+    if re.search("http://", link.get('href')):
+        print link.get('href')
+    else:
+        print "http://" + userCiti + ".craigslist.org" + link.get('href')
 print "\n"
 
 """ 
