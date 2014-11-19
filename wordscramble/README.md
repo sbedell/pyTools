eb28b037e09d792c821e4d61efccc9b59932a4d7
Steve B <bedell.369@osu.edu>
2014-11-19 00:11:40 -0500

    minor fixes in wordscramble

---
+++ wordscramble/README.md
@@ -0,0 +1,19 @@
+wordscramble
+=======
+
+This script takes a wordlist or sentence and outputs it scrambled like the previous sentence.
+
+Stcineitss say ppeloe can raed snenteces lkie tihs as lnog as the fsirt and lsat leertts are in the rhgit palce.
+
+Usage:
+------
+-h displays help
+-f scrambles a file
+-s scrambles a sentence, takes in a string value
+This will scramble the sourcecode:
+$ python -f wordscramble.py
+
+$ python -s "This will scramble this sentence"
+Example output: "Tihs wlil scrbamle tihs setncene"
+
+Check out the worldlists folder for some samples to scramble.
--- wordscramble/wordscramble.py
+++ wordscramble/wordscramble.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python
 
-# make words into wdros and shit like that
+# make words into wdros and shit like taht
 # Usage: python wordscramble.py -h
-# Easter Egg: 'python wordscramble.py -f wordscramble.py'
-#           This will scramble this file :D.
 
-import optparse, sys, random, math
+import optparse, sys, random, math, argparse
 
 """ Returns the scrambled word. """
 def scrambleWord(word):
     if len(word) < 3:       # cannot be scrambled
         return word
     elif len(word) == 4:    # scramble two middle letters        
-        # 85% chance to NOT scramble a word       
-        if random.random() > 0.80:
+        # 85% chance to NOT scramble the word       
+        if random.random() > 0.85:
             return word
-        else:               # return the middle two chars swapped
+        else:    # return the middle two chars swapped
             return word[0] + word[2] + word[1] + word[3]
     else:
         newWord = word[0]   # start with the first letter
         letterList = list(word[1:-1])
-        iterations = len(letterList)
-        for i in range(iterations):
+        for i in range(len(letterList)):
             rando = int(math.floor(random.random() * len(letterList)))
             #print rando
             newWord += letterList[rando]
@@ -35,26 +32,28 @@ def scrambleWord(word):
 #Start Main Program:
 
 # Create option parser:
-parser = optparse.OptionParser('python wordscramble.py <options>')
+parser = optparse.OptionParser('python wordscramble.py <options>\n[-f file]\n[-s string]\n[-h help]')
 parser.add_option('-f', dest='file', type='string', help='Specify file to scramble')
-parser.add_option('-s', dest='sentence', type='string', help='Specify a sentence to scramble. Note: put quotes around this sentence or else you will get errors.', default='This is a test sentence')
+parser.add_option('-s', dest='sentence', type='string', help='Specify a word or sentence to scramble. Note: put quotes around this sentence or else you will get errors.', default='This is a test sentence')
 
 (options, args) = parser.parse_args()
 
-# No paramaters given, print usage and exit
-if not options.file and not options.sentence:
+# No parameters given, print usage and exit
+if not options.file and options.sentence == 'This is a test sentence':
     print parser.print_usage()
-    sys.exit(-1)
+    sys.exit(1)
 
-if options.sentence:
-    print
+if options.sentence and (options.sentence != 'This is a test sentence' and not options.file):
+    print     # blank line print
     for word in str(options.sentence).replace('.', ' ').split():
         print scrambleWord(word), 
     print "\n"
 
 if options.file:
+    print
     with open(options.file, 'r') as f:
         for line in f:
             for word in str(line).replace('.', ' ').strip().split():
                 print scrambleWord(word),
             print
+    print
