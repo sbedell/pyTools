443fa2a2adaf7e90edbd701bc3c365a9ad47578e
Steve B <bedell.369@osu.edu>
2021-06-24 14:27:41 -0400

    Updating unit tests in password-generator-tests.py

--- password-generator-tests.py
+++ password-generator-tests.py
@@ -2,25 +2,37 @@ import unittest
 import password_generator
 
 class TestPasswordGenerator(unittest.TestCase):
-  #def setUp(self):
+  def setUp(self):
+    self.mystr = "teststring"
+    self.pickChars = password_generator.pickCharsFromString(self.mystr, 5)
+    self.shuffledString = password_generator.shuffleString(self.mystr)
 
-  def testPickCharsFromString(self):
-    mystr = "teststring"
-    pickChars = password_generator.pickCharsFromString(mystr, 3)
+  def testPickCharsFromStringLength(self):
     # Assert that the length is correct:
-    self.assertEqual(len(pickChars), 3)
+    self.assertEqual(len(self.pickChars), 5)
 
+  def testPickCharsCharacters(self):
     # Assert that each chosen character was in the original string:
-    for c in pickChars:
-      self.assertTrue(c in mystr)
+    for c in self.pickChars:
+      self.assertTrue(c in self.mystr)
 
   def testShuffleString(self):
-    mystr = "teststring"
-    shuffledString = password_generator.shuffleString(mystr)
-    self.assertNotEqual(mystr, shuffledString)
+    # Assert that the shuffle actually produces a different string:
+    # (I guess there is some % chance that the shuffle produces the exact same string, but that's gonna be rare.)
+    self.assertNotEqual(self.mystr, self.shuffledString)
+
+  def testShuffleStringChars(self):
+    # Assert that all the same chars are in the shuffled string. (Why wouldn't this pass?)
+    for c in self.shuffledString:
+      self.assertTrue(c in self.mystr)
+  
+  def testShuffleLength(self):
+    # Assert that the shuffled string is the same length as the original:
+    self.assertEqual(len(self.mystr), len(self.shuffledString))
 
   def testCorrectPasswordLength(self):
     passw = password_generator.generateRandomPassword(15)
+    # Assert that generateRandomPassword produces a password of correct given length:
     self.assertEqual(len(passw), 15)
 
 if __name__ == '__main__':
